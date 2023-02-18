2e4efc31c872538eb5771a3d35f9c2cefced7114
Ziru Zou <jiyucoding@yahoo.com>
2023-02-18 16:13:56 -0500

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -21,6 +21,7 @@ def square_integers(int_list):
 
 def fizzbuzz():
     for i in range(1, 101):
+        # if i % 15 == 0:
         if not i % 15:
             print("FizzBuzz")
         elif not i % 5:
